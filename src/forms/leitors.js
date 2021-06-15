94059d6b18f286ec07f2e44ed8227c5a9e8ac323
Nicolau NP <mr1Normaliii@gmail.com>
2021-06-15 20:28:57 +0100

    terminado a criacao de formulario

--- src/forms/leitors.js
+++ src/forms/leitors.js
@@ -7,16 +7,32 @@ const LeitorForm = () => {
     const [genero, setGenero] = useState("")
     const [bi, setBi] = useState("")
     const [foto, setFoto] = useState("")
+    const [estado, setEstado] = useState("on")
 
     /*dados leitor*/
     const [telefone, setTelefone] = useState("")
     const [bairro, setBairro] = useState("")
 
 
-    
+
+
     return (
         <div>
             <h3>Formulario de Cadastro de Leitor</h3>
+            <form>
+               
+               <input type="text" name="nome" required="required" placeholder="Nome" /><br/>
+               <select name="genero" required>
+                    <option value="">Genero</option>
+                   <option value="M">M</option>
+                   <option value="F">F</option>
+               </select><br/>
+                <input type="text" name="bi" placeholder="Bilhete de Identidade"/><br/>
+                <input type="file" name="foto"/><br/>
+
+                <input type="number" name="telefone" required="required" placeholder="Telefone" /><br/>
+                <input type="text" name="bairro" required="required" placeholder="Bairro" /><br/>
+            </form>
         </div>
     )
 }
