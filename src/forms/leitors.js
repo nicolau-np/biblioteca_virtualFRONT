b6be5a694019004d99b56251f565b485bdd29b59
Nicolau NP <mr1Normaliii@gmail.com>
2021-06-15 20:35:14 +0100

    criando o link dos inputs com as respectivas variaveis

--- src/forms/leitors.js
+++ src/forms/leitors.js
@@ -14,24 +14,40 @@ const LeitorForm = () => {
     const [bairro, setBairro] = useState("")
 
 
-
+    const submit=()=>{
+        
+    }
 
     return (
         <div>
             <h3>Formulario de Cadastro de Leitor</h3>
             <form>
                
-               <input type="text" name="nome" required="required" placeholder="Nome" /><br/>
-               <select name="genero" required>
+               <input type="text" name="nome" required="required" placeholder="Nome" 
+               onChange={(e)=>setNome(e.target.value)}
+               /><br/>
+               <select name="genero" required="required" 
+               onChange={(e)=>setGenero(e.target.value)}
+               >
                     <option value="">Genero</option>
                    <option value="M">M</option>
                    <option value="F">F</option>
                </select><br/>
-                <input type="text" name="bi" placeholder="Bilhete de Identidade"/><br/>
-                <input type="file" name="foto"/><br/>
+                <input type="text" name="bi" placeholder="Bilhete de Identidade" 
+                onChange={(e)=>setBi(e.target.value)}
+                /><br/>
+                <input type="file" name="foto"
+                onChange={(e)=>setFoto(e.target.files[0])}
+                /><br/>
 
-                <input type="number" name="telefone" required="required" placeholder="Telefone" /><br/>
-                <input type="text" name="bairro" required="required" placeholder="Bairro" /><br/>
+                <input type="number" name="telefone" required="required" placeholder="Telefone" 
+                onChange={(e)=>setTelefone(e.target.value)}
+                /><br/>
+                <input type="text" name="bairro" required="required" placeholder="Bairro" 
+                onChange={(e)=>setBairro(e.target.value)}
+                /><br/>
+
+                <button onClick={submit}>Enviar</button>
             </form>
         </div>
     )
