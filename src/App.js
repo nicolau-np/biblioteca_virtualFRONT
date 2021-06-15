c45766cfad29824ddfc65e00233427abeb8d43ba
Nicolau NP <mr1Normaliii@gmail.com>
2021-06-15 21:44:26 +0100

    mudando a pagina default

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@
 import React from 'react'
 import Contador from './components_example/Contador'
 import FormLeitor from './forms/leitors'
+import ListLeitor from './lists/leitors'
 function App() {
   return (
     <div className="App">
      <h1>Biblioteca Virtual</h1> 
-      <FormLeitor/>
+      <ListLeitor/>
     </div>
   );
 }
