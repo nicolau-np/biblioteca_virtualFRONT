fd502095932b2b92442b148e07990150321fd1f9
Nicolau NP <mr1Normaliii@gmail.com>
2021-06-15 20:00:44 +0100

    adicionando status

--- src/components/Contador.js
+++ src/components/Contador.js
@@ -3,16 +3,23 @@ import React, {useState, useEffect} from 'react'
 const Contador = ()=> {
 
     const [count, setCount] = useState(0);
+    const [status, setSatus] = useState('');
 
     const increment=()=>{
-        console.log("click em aumentar")
-        setCount(count+1)
+        if(count==30){
+            setStatus("Chegou o fim da contagem")
+        }else{
+           console.log("click em aumentar")
+            setCount(count+1) 
+        }
+        
     }
 
     return (
         <div>
-            <h3>Contador: {count}</h3>
+            <h3>Contador: {count}%</h3>
             <button type="button" onClick={increment}>Aumentar</button>
+            <span style={{color:red}}>{status}</span>
         </div>    
     );
 }
