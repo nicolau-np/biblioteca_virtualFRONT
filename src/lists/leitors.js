1d4e0fb8c4bbefdd659c3846c0fd0066a691d4b8
Nicolau NP <mr1Normaliii@gmail.com>
2021-06-15 22:08:56 +0100

    preenchendo a minha lista com arequisicao

--- src/lists/leitors.js
+++ src/lists/leitors.js
@@ -1,10 +1,30 @@
-import React, {useSatate} from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 const LeitorList = ()=> {
 
+    const [leitors, setLeitors] = useState([])
+
+    useEffect(()=>{
+        axios.get('http://localhost:8000/api/leitors').
+        then((response)=>{
+            console.log(response.data.data)
+            setLeitors(response.data.data)
+        })
+    }, [])
+   
+
     return (
     <div>
-      <h3>Lista de Leitores</h3><br/>  
+      <h3>Lista de Leitores</h3><br/> 
+
+        <div className="card">
+            <img src="none.jpg" alt="" style={{width:'200px', height:'130px'}}/>
+            <div className="description">
+               Hello world
+            </div>
+        </div>
+
     </div>
     )
 }
