6dc5aa1b6781e4e0d545a976e40656e2b946a8ff
Nicolau NP <mr1Normaliii@gmail.com>
2021-06-15 20:20:01 +0100

    criando as variaveis para os formularios

--- src/forms/leitors.js
+++ src/forms/leitors.js
@@ -2,7 +2,18 @@ import React, {useState} from 'react'
 
 const LeitorForm = () => {
 
+    /*dados pessoais*/
+    const [nome, setNome] = useState("")
+    const [genero, setGenero] = useState("")
+    const [bi, setBi] = useState("")
+    const [foto, setFoto] = useState("")
 
+    /*dados leitor*/
+    const [telefone, setTelefone] = useState("")
+    const [bairro, setBairro] = useState("")
+
+
+    
     return (
         <div>
             <h3>Formulario de Cadastro de Leitor</h3>
