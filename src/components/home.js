66f61d0872252d1031387f38f7ee6d22e267d9e2
Nicolau NP <mr1Normaliii@gmail.com>
2021-06-16 07:57:51 +0100

    chamando os includes no componente mae

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import Header from './../include/header'
+import Footer from './../include/footer'
+import Menu from './../include/menu'
+
+
+const Content = () => {
+    return ( 
+    <Fragment>
+            <h2>Home</h2>
+    </Fragment> 
+    );
+}
+ 
+
 
 const Home = () => {
     return (  
-        <div>
-            <h2>Home</h2>
-        </div>
+        <Fragment>
+            <Header/>
+            <Menu/>
+            <Footer/>
+        </Fragment>
     );
 }
  
