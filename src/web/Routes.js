fe6515db1dc529598e68360011dee48833954494
Nicolau NP <mr1Normaliii@gmail.com>
2021-06-17 19:33:11 +0100

    adicionando pagina de erro

--- src/web/Routes.js
+++ src/web/Routes.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import {Switch, Route} from 'react-router-dom'
 
-import Home from '../components/home'
-import Sobre from '../components/sobre'
+import Home from './../components/home'
+import Sobre from './../components/sobre'
+import Error404 from './../errors/e404'
 
  const Routes=()=>{
     return (
         <Switch>
             <Route exact path="/" component={Home}/>
             <Route exact path="/sobre" component={Sobre}/>
+            <Route path="*" component={Error404}/>
         </Switch>
     );
 }
