c03cd6c4a5a94c0e2f428347c9615c8376715566
Nicolau NP <mr1Normaliii@gmail.com>
2021-06-17 20:17:09 +0100

    criando tela de login

--- src/components/login.js
+++ src/components/login.js
@@ -6,7 +6,11 @@ const Login = () => {
     const [password, setPassword] = useState([])
 
     function submit(){
-        
+        const formData = new FormData()
+        formData.append('email', email)
+        formData.append('password', password)
+
+        console.log("logou aqui")
     }
 
     return (  
@@ -14,8 +18,10 @@ const Login = () => {
             <form>
                 <input type="email" name="email" 
                 onChange={(e)=>setEmail(e.target.value)}
+                value={email}
                 required /><br/>
                 <input type="password" name="password"
+                value={password}
                 onChange={(e)=>setPassword(e.target.value)}
                 required/><br/>
                 <button type="button" onClick={submit}>Entrar</button>
