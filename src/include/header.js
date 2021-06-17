1317f61342d69c5bee3bd479aecd0b17710d89c3
Nicolau NP <mr1Normaliii@gmail.com>
2021-06-17 20:09:36 +0100

    criando login file

--- src/include/header.js
+++ src/include/header.js
@@ -13,6 +13,7 @@ const Header = () => {
                 <ul>
                     <li><a href="/">Home</a></li>
                     <li><a href="/autores">Autores</a></li>
+                    <li><a href="/login">Login</a></li>
                     <li><a href="/sobre">Sobre</a></li>
                 </ul>
             </div>
