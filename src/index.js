81057d27c7de774a0ffa5fc1723aa9e01202709d
Nicolau NP <mr1Normaliii@gmail.com>
2021-06-18 21:00:09 +0100

    removendo pacotes bootstrap

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import reportWebVitals from './reportWebVitals';
 
 import './assets/css/style.css'
 
-import './assets/bootstrap-5/css/bootstrap.min.css'
-import './assets/bootstrap-5/js/bootstrap.min.js'
 
 ReactDOM.render(
   <React.StrictMode>
