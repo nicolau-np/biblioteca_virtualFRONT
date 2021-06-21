011cd72eaa45498d0309c292f4622e0404f121d1
Nicolau NP <mr1Normaliii@gmail.com>
2021-06-22 00:46:40 +0100

    criando componente de register

---
+++ src/components/register.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+
+const Register = () => {
+    return ( 
+        <Fragment>
+            
+        </Fragment>
+     );
+}
+ 
+export default Register;
--- src/web/Routes.js
+++ src/web/Routes.js
@@ -3,6 +3,7 @@ import {Switch, Route} from 'react-router-dom'
 
 import Login from '../components/login'
 import Home from './../components/home'
+import Register from './../components/register'
 import Sobre from './../components/sobre'
 import Error404 from './../errors/e404'
 
@@ -11,6 +12,7 @@ import Error404 from './../errors/e404'
         <Switch>
             <Route exact path="/" component={Home}/>
             <Route exact path="/login" component={Login}/>
+            <Route exact path="/register" component={Register}/>
             <Route exact path="/sobre" component={Sobre}/>
             <Route path="*" component={Error404}/>
         </Switch>
