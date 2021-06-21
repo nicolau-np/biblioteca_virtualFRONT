a7627ca1217f896443ea54091639562e405f1149
Nicolau NP <mr1Normaliii@gmail.com>
2021-06-22 00:07:51 +0100

    passando o metodo de logout no header

--- src/include/header.js
+++ src/include/header.js
@@ -1,7 +1,29 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import {Link, useHistory} from 'react-router-dom'
 
 const Header = () => {
+
+    const logout = async () => {
+        const history = useHistory()
+
+         const response = await fetch('http://127.0.0.1:8000/api/users/logout', {
+            headers: {
+               'Content-Type':'application/json',
+               Authorization: 'bearer ' + window.localStorage.getItem('token')
+            },
+        })
+
+        const content = await response.json()
+        if(content.status=="success"){
+            return history.push('/login')
+            //window.localStorage.removeItem('token')
+        }else{
+          return history.push('/')
+        }
+    }
+
+
+
     return ( 
     <div className="container">
         <div className="header">
@@ -15,7 +37,7 @@ const Header = () => {
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/autores">Autores</Link></li>
                     <li><Link to="/login">Login</Link></li>
-                    <li><Link to="/logout">Logout</Link></li>
+                    <li><Link to="#" onClick={logout}>Logout</Link></li>
                     <li><Link to="/sobre">Sobre</Link></li>
                 </ul>
             </div>
