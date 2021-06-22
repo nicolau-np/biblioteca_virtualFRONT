2e304f6dec75a8c3d8d8c417742695c030b62f95
Nicolau NP <mr1Normaliii@gmail.com>
2021-06-22 01:11:51 +0100

    terminado metodo de register

--- src/components/register.js
+++ src/components/register.js
@@ -7,12 +7,36 @@ const Register = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [acesso, setAcesso] = useState('')
+    const [foto, setFoto] = useState('')
     const [estadoU, setEstadoU] = useState('on')
     const [status, setStatus] = useState('')
 
     const submit = async (e) => {
         e.preventDefault()
         
+        const response = await fetch('http://127.0.0.1:8000/api/users/register', {
+            method: 'POST',
+            headers: {'Content-Type':'application/json'},
+            
+            body: JSON.stringify({
+                nome,
+                genero,
+                estadoP,
+                email,
+                password,
+                acesso,
+                foto,
+                estadoU,
+            })
+        })
+
+        const content = await response.json()
+        if(content.status === "success"){
+            setStatus(content.data)
+            console.log(content)
+        }else{
+            console.log(content)
+        }
     }
 
 
@@ -30,6 +54,9 @@ const Register = () => {
                     <option value="M">M</option>
                     <option value="F">F</option>
                 </select><br/>
+                <input type="file" name="foto" value=""
+                onChange={(e)=>setFoto(e.target.value)}
+                /><br/>
                 <input type="email" name="email" value=""
                 onChange={(e)=>setEmail(e.target.value)}
                 /><br/>
