c146be98da64d718355927f40e7f37d4ea6dfc6c
Nicolau NP <mr1Normaliii@gmail.com>
2021-06-22 09:17:27 +0100

    adicionando novos pacotes do bootstrap e jquery

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'bootstrap/dist/css/bootstrap.css'
+import $ from 'jquery'
+import Popper from 'popper.js'
+import 'bootstrap/dist/js/bootstrap.bundle'
+
 import './assets/css/style.css'
 
 
