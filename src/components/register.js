2e60915d388dbdff0fd879c51a5731e38d18a1c2
Nicolau NP <mr1Normaliii@gmail.com>
2021-06-22 01:03:24 +0100

    criando metodo de register

--- src/components/register.js
+++ src/components/register.js
@@ -1,4 +1,4 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 
 const Register = () => {
     const [nome, setNome] = useState('')
@@ -8,23 +8,37 @@ const Register = () => {
     const [password, setPassword] = useState('')
     const [acesso, setAcesso] = useState('')
     const [estadoU, setEstadoU] = useState('on')
+    const [status, setStatus] = useState('')
 
-
+    const submit = async (e) => {
+        e.preventDefault()
+        
+    }
 
 
     return ( 
         <Fragment>
-            <form>
+            <br/><br/><br/>
+            {status}
+            <form onSubmit={submit}>
                 <input type="text" name="nome" value=""
                 onChange={(e)=>setNome(e.target.value)}
                 /><br/>
-                <select name="genero">
+                <select name="genero"
+                onChange={(e)=>setGenero(e.target.value)}
+                >
                     <option value="M">M</option>
                     <option value="F">F</option>
                 </select><br/>
-                <input type="email" name="email" value=""/><br/>
-                <input type="password" name="password" value=""/><br/>
-                <select name="acesso">
+                <input type="email" name="email" value=""
+                onChange={(e)=>setEmail(e.target.value)}
+                /><br/>
+                <input type="password" name="password" value=""
+                onChange={(e)=>setPassword(e.target.value)}
+                /><br/>
+                <select name="acesso"
+                onChange={(e)=>setAcesso(e.target.value)}
+                >
                     <option value="admin">admin</option>
                     <option value="user">user</option>
                 </select><br/>
